b1ab051833fec63e06fa416dffe882a928ac7ffc
Jonas Colve <jonas.colve@outlook.de>
2020-04-22 18:49:40 +0200

    Added solution to Exercise 2.5.1

--- Exercises/Exercise 2/ex2_probs.py
+++ Exercises/Exercise 2/ex2_probs.py
@@ -1,6 +1,21 @@
 # global imports
 import numpy as np
 
+def emp_mean_cov( samples ):
+    n = len(samples)
+    mean = np.sum( samples, axis=0 )/n
+    cov_k = lambda x: [[ x[i]*x[j] for i in range(len(x))] for j in range(len(x)) ]
+    cov = np.sum( np.array( list(map( cov_k, samples-mean )) ), axis=0 )/n
+    return mean, cov
+
+def theo_mean_cov( states, probs ):
+    n = len(states)
+    mean = np.sum( probs[:,None] * states, axis=0 )
+    cov_k = lambda x: [[ x[i]*x[j] for i in range(len(x))] for j in range(len(x)) ]
+    cov = np.sum( probs[:,None,None] * np.array( list(map( cov_k, states-mean )) ), axis=0 )
+    return mean, cov
+
+
 ## define parameters
 n = 1e5                                                       ## number of samples
 states = np.array([ [0.,0.] , [0.,1.] , [1.,0.] , [1.,1.] ])  ## possible states
@@ -11,7 +26,7 @@ probs /= np.sum(probs)                                        ## normalisation
 
 ## generate random samples from distribution
 samples = []
-for cs in xrange(int(n)):
+for cs in range(int(n)):
     l = np.random.rand()
     i = 0
     while np.sum(probs[:i]) < l:
@@ -23,19 +38,24 @@ samples = np.array(samples)
 
 ## empirical statistics
 
-# YOUR CODE HERE
+means, cov = emp_mean_cov(samples)
+mean_z1, mean_z2 = means
+(var_z1, cov_z1z2), (_, var_z2) = cov
 
 ## theoretical statistics
 
-# YOUR CODE HERE
+means_theo, cov_theo = theo_mean_cov(states,probs)
+mean_z1_theo, mean_z2_theo = means_theo
+(var_z1_theo, cov_z1z2_theo), (_, var_z2_theo) = cov_theo
 
 ##########################################################################
 
 ## print the results
-print
-print "              \t empirical \t theoretical"
-print "Mean(z1)    : \t %.4f      \t %.4f       " % (mean_z1, mean_z1_theo)
-print "Mean(z2)    : \t %.4f      \t %.4f       " % (mean_z2, mean_z2_theo)
-print "Var(z1)     : \t %.4f      \t %.4f       " % (var_z1, var_z1_theo)
-print "Var(z2)     : \t %.4f      \t %.4f       " % (var_z2, var_z2_theo)
-print "Cov(z1,z2)  : \t %.4f      \t %.4f       " % (cov_z1z2, cov_z1z2_theo)
+print("n = {:.1e}".format(n))
+print()
+print("              \t empirical \t theoretical")
+print("Mean(z1)    : \t {:.4f}      \t {:.4f}       ".format(mean_z1, mean_z1_theo))
+print("Mean(z2)    : \t {:.4f}      \t {:.4f}       ".format(mean_z2, mean_z2_theo))
+print("Var(z1)     : \t {:.4f}      \t {:.4f}       ".format(var_z1, var_z1_theo))
+print("Var(z2)     : \t {:.4f}      \t {:.4f}       ".format(var_z2, var_z2_theo))
+print("Cov(z1,z2)  : \t {:.4f}      \t {:.4f}       ".format(cov_z1z2, cov_z1z2_theo))
